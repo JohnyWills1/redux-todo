ac646da837663c825eca96d9d8d5a6822a9305af
Jonathan Wills <jonathan_wills@icloud.com>
2022-08-16 17:04:57 +0900

    feat: create the main redux store

---
+++ src/store/store.ts
@@ -0,0 +1,23 @@
+// #region Global Imports
+import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch as useDispatchBase, useSelector as useSelectorBase } from 'react-redux';
+// #endregion Global Imports
+
+// #region Local Imports
+import todosSlice from './slices/todosSlice';
+// #endregion Local Imports
+
+export const store = configureStore({
+  reducer: {
+    todos: todosSlice,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+type AppDispatch = typeof store.dispatch;
+
+export const useDispatch = () => useDispatchBase<AppDispatch>();
+
+export const useSelector = <TSelected = unknown>(selector: (state: RootState) => TSelected): TSelected =>
+  useSelectorBase<RootState, TSelected>(selector);
