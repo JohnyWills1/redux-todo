19a2431e92d7d75ddb516101c5c091dc7e90c1d8
Jonathan Wills <jonathan_wills@icloud.com>
2022-08-18 16:39:48 +0900

    feat: update todo item list with new todo item component

--- src/Components/TodoItemList/TodoItemList.tsx
+++ src/Components/TodoItemList/TodoItemList.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { ToDo } from '../../types';
+import TodoItem from '../TodoItem/TodoItem';
 
 type Props = {
-  todos?: ToDo[];
+    todos?: ToDo[];
 };
 
 function TodoItemList({ todos }: Props): JSX.Element {
-  return <div>{todos ? todos.map((t) => <p key={t.id}>{t.text}</p>) : <p>No Todo&apos;s set</p>}</div>;
+    return (
+        <div>
+            {todos && todos.length > 0 ? (
+                todos.map((t) => <TodoItem key={t.id} todo={t} />)
+            ) : (
+                <p>Empty!</p>
+            )}
+        </div>
+    );
 }
 
 export default TodoItemList;
