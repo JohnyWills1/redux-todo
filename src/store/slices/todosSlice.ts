1ad4642e080f8565404a626aac6ad9a20a81fc32
Jonathan Wills <jonathan_wills@icloud.com>
2022-08-18 16:39:58 +0900

    feat: add new reducers and state to todos slice

--- src/store/slices/todosSlice.ts
+++ src/store/slices/todosSlice.ts
@@ -7,45 +7,83 @@ import { ToDo } from '../../types';
 // #endregion Local Imports
 
 export interface TodosState {
-  todos?: ToDo[];
+    todos?: ToDo[];
+    filter: 'all' | 'done' | 'incomplete';
 }
 
 const initialState: TodosState = {
-  todos: undefined,
+    todos: undefined,
+    filter: 'all',
 } as const;
 
 export const todosSlice = createSlice({
-  name: 'todos',
-  initialState,
-  reducers: {
-    addTodo: (state: Draft<typeof initialState>, action: PayloadAction<ToDo>) => {
-      if (state.todos !== undefined) {
-        return { todos: [...state.todos, action.payload] };
-      }
-
-      return { todos: [action.payload] };
-    },
-    toggleTodo: (state: Draft<typeof initialState>, action: PayloadAction<ToDo>) => {
-      if (state.todos !== undefined) {
-        return {
-          todos: state.todos.map((t) => {
-            if (t.id !== action.payload.id) {
-              return t;
+    name: 'todos',
+    initialState,
+    reducers: {
+        addTodo: (
+            state: Draft<typeof initialState>,
+            action: PayloadAction<ToDo>,
+        ) => {
+            if (state.todos !== undefined) {
+                if (action.payload.text !== '') {
+                    return {
+                        ...state,
+                        todos: [...state.todos, action.payload],
+                    };
+                }
+                return;
             }
 
+            return { ...state, todos: [action.payload] };
+        },
+        toggleTodo: (
+            state: Draft<typeof initialState>,
+            action: PayloadAction<ToDo>,
+        ) => {
+            if (state.todos !== undefined) {
+                return {
+                    ...state,
+                    todos: state.todos.map((t) => {
+                        if (t.id !== action.payload.id) {
+                            return t;
+                        }
+
+                        return {
+                            ...t,
+                            done: !t.done,
+                        };
+                    }),
+                };
+            }
+        },
+        toggleAllTodos: (state: Draft<typeof initialState>) => {
+            if (state.todos !== undefined) {
+                return {
+                    ...state,
+                    todos: state.todos.map((t) => {
+                        return {
+                            ...t,
+                            done: true,
+                        };
+                    }),
+                };
+            }
+        },
+        toggleSelectedFilter: (
+            state: Draft<typeof initialState>,
+            action: PayloadAction<'all' | 'done' | 'incomplete'>,
+        ) => {
             return {
-              ...t,
-              done: !t.done,
+                ...state,
+                filter: action.payload,
             };
-          }),
-        };
-      }
+        },
     },
-  },
 });
 
 export const getTodosState = (state: { todos: TodosState }) => state.todos;
 
-export const { addTodo, toggleTodo } = todosSlice.actions;
+export const { addTodo, toggleAllTodos, toggleSelectedFilter, toggleTodo } =
+    todosSlice.actions;
 
 export default todosSlice.reducer;
