3f412714823139abff628dd0f4dad8fc1082f078
Jonathan Wills <jonathan_wills@icloud.com>
2022-08-18 16:40:06 +0900

    feat: update button component

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 type Props = {
-  buttonText: string;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  shape: 'square' | 'pill';
-  type: 'primary' | 'secondary';
+    buttonText: string;
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    shape: 'square' | 'pill';
+    kind: 'primary' | 'secondary';
 };
 
-function Button({ buttonText, onClick = () => {}, shape, type }: Props): JSX.Element {
-  const ButtonComponent = styled.button`
+const ButtonComponent = styled.button<Props>`
     font-size: 1em;
     margin: 1em;
     padding: 0.25em 1em;
-    color: ${type === 'primary' ? 'black' : 'white'};
-    background-color: ${type === 'primary' ? 'white' : 'transparent'};
-    border: ${type === 'primary' ? 'none' : '2px solid white'};
-    border-radius: ${shape === 'square' ? '3px' : '16px'};
+    color: ${(props) => (props.kind === 'primary' ? 'black' : 'white')};
+    background-color: ${(props) =>
+        props.kind === 'primary' ? 'white' : 'transparent'};
+    border: ${(props) =>
+        props.kind === 'primary' ? 'none' : '2px solid white'};
+    border-radius: ${(props) => (props.shape === 'square' ? '3px' : '16px')};
     cursor: pointer;
-  `;
+`;
 
-  return <ButtonComponent onClick={onClick}>{buttonText}</ButtonComponent>;
+function Button({
+    buttonText,
+    onClick = () => {},
+    shape,
+    kind,
+}: Props): JSX.Element {
+    return (
+        <ButtonComponent
+            onClick={onClick}
+            shape={shape}
+            kind={kind}
+            buttonText={buttonText}
+        >
+            {buttonText}
+        </ButtonComponent>
+    );
 }
 
 export default Button;
